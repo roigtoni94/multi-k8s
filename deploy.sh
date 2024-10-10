85b206a4e593ec1d854a202afd04e08515ba80cd
roigtoni94 <roigtoni94@gmail.com>
2024-10-10 14:07:51 +0200

    Added dployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t roigtoni94/multi-client-k8s:latest -t roigtoni94/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t roigtoni94/multi-server-k8s:latest -t roigtoni94/multi-server-k8s:$SHA -f ./server/Dockerfile ./server
-docker build -t roigtoni94/multi-worker-k8s:latest -t roigtoni94/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t roigtoni94/multi-client:latest -t roigtoni94/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t roigtoni94/multi-server:latest -t roigtoni94/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t roigtoni94/multi-worker:latest -t roigtoni94/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push roigtoni94/multi-client-k8s:latest
-docker push roigtoni94/multi-server-k8s:latest
-docker push roigtoni94/multi-worker-k8s:latest
+docker push roigtoni94/multi-client:latest
+docker push roigtoni94/multi-server:latest
+docker push roigtoni94/multi-worker:latest
 
-docker push roigtoni94/multi-client-k8s:$SHA
-docker push roigtoni94/multi-server-k8s:$SHA
-docker push roigtoni94/multi-worker-k8s:$SHA
+docker push roigtoni94/multi-client:$SHA
+docker push roigtoni94/multi-server:$SHA
+docker push roigtoni94/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=roigtoni94/multi-server-k8s:$SHA
-kubectl set image deployments/client-deployment client=roigtoni94/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=roigtoni94/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=roigtoni94/multi-server:$SHA
+kubectl set image deployments/client-deployment client=roigtoni94/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=roigtoni94/multi-worker:$SHA
